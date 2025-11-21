bc9f9482e5b668e968e3cb827f0dbaa22e19cc7b
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2025-11-21 06:57:33 +0530

    updated confirm exit app

--- lib/payment/suc.dart
+++ lib/payment/suc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PaymentSuccessPage extends StatelessWidget {
+  const PaymentSuccessPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,18 +13,11 @@ class PaymentSuccessPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Icon(
-                Icons.check_circle,
-                color: Colors.green,
-                size: 100,
-              ),
+              Icon(Icons.check_circle, color: Colors.green, size: 100),
               SizedBox(height: 20),
               Text(
                 "Payment Successful!",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 10),
